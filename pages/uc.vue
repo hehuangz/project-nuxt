df46ec9137eacda89f6ead2d24fff5e31f69d4bd
hehuangz <1025005090@qq.com>
2020-11-12 15:16:48 +0800

    add: 完善登陆失效逻辑

---
+++ pages/uc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <h1>用户中心</h1>
+      <i class="el-icon-loading"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  async mounted () {
+    const ret = await this.$http.get('/user/info')
+    console.log(ret, 12)
+  }
+}
+</script>
--- plugins/axios.js
+++ plugins/axios.js
@@ -1,17 +1,52 @@
 import Vue from 'vue'
 import axios from 'axios'
+import { MessageBox } from 'element-ui'
 let service = axios.create({
   timeout: 5000,
   // 前缀
   baseURL: '/api'
 })
+const TOKEN_KEY = 'USER_TOKEN'
+export default({ store, redirect }) => {
+  service.interceptors.request.use(
+    config=>{
+      // 请求加token
+      const token = window.localStorage.getItem(TOKEN_KEY)
+      // 设置url白名单
+      if(token){
+        // 加个Bearer规范
+        config.headers.common['Authorization'] = 'Bearer '+token
+      }
+      return config
+    },
+    err=>{
+      return Promise.reject(err)
+    }
+  )
+  service.interceptors.response.use(
+    async response => {
+      let { data, config } = response
+      if(data.code === 0){
+        if(config.url === '/user/login'){
+          localStorage.setItem(TOKEN_KEY, data.data.token)
+        }
+      }else if(data.code === -666){
+        // code是-666的 意味着token过期
+        // @todo
+        MessageBox.confirm('登录过期了', '过期', {
+          confirmButtonText: '登录',
+          showCancelButton: false,
+          type: 'warning'
+        }).then(() => {
+          localStorage.removeItem(TOKEN_KEY)
+          redirect({ path:'/login' })
+        })
+      }
+      return data
+    }
+  )
+}
 
-service.interceptors.response.use(
-  async response => {
-    let { data, config } = response
-    return data
-  }
-)
 Vue.prototype.$http = service
 
 export const http = service
